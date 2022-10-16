d61ebd7c4b82b28f30d5f54d62c85be3664ea852
jbergthold <jbergthold@uiowa.edu>
2022-10-16 04:01:05 -0500

    did things

--- src/front/main.py
+++ src/front/main.py
@@ -1,8 +1,16 @@
-import datetime
 
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import mpld3
 
 def main():
-    print("Hello");
+    s = pd.Series([1, 2, 3])
+    fig, ax = plt.subplots()
+    s.plot.bar()
+    fig.savefig('my_plot.png')
+
+
 
 
 if (__name__ == "__main__"):
